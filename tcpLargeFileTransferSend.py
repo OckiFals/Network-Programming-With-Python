407316a9b0dc49a9f57ff15a57fba186ebe74a9d
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2016-03-09 16:39:39 +0700

    Check file size before transmit over TCP socket

--- tcpLargeFileTransferSend.py
+++ tcpLargeFileTransferSend.py
@@ -13,6 +13,7 @@ class TcpLargeFileTransferSend:
     def __init__(self):
         self.targetip = '127.0.0.1' if len(sys.argv) is not 2 else sys.argv[1]
         self.targetport = '8080' if len(sys.argv) is not 3 else sys.argv[2]
+        self.filetotransport = 'Fedora-Live-Workstation-x86_64-23-10.iso'
 
     def createconnection(self):
         tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,6 +34,13 @@ class TcpLargeFileTransferSend:
         sock.close()
 
     def run(self):
+        with open(self.filetotransport, 'rb') as infile:
+            infile.seek(0, 2)
+            size = infile.tell()
+
+        if 1024000 > size:
+            raise Exception('file must larger than 1GB')
+
         self.sendfile()
 
 
